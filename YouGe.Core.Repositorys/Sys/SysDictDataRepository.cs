f7778815878221f5f0dd8cf02b08356fd3cc9ddd
chen qiang <chenqiangdage@foxmail.com>
2020-09-06 22:27:42 +0800

    sys dict

--- YouGe.Core.Repositorys/Sys/SysDictDataRepository.cs
+++ YouGe.Core.Repositorys/Sys/SysDictDataRepository.cs
@@ -1,10 +1,58 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YouGe.Core.Interface.IRepositorys.Sys;
+using YouGe.Core.DBEntitys.Sys;
+using YouGe.Core.Interface.IDbContexts;
+using YouGe.Core.Models.DTModel.Sys;
+using YouGe.Core.Common.YouGeException;
+using YouGe.Core.Commons.Helper;
+using YouGe.Core.Common.SystemConst;
+using YouGe.Core.Models.System;
+using YouGe.Core.Common.Helper;
+using System.Threading.Tasks;
+using YouGe.Core.DbContexts;
+using System.Linq.Expressions;
+using YouGe.Core.Commons;
+using MySql.Data.MySqlClient;
+using System.Linq;
+
 namespace YouGe.Core.Repositorys.Sys
 {
-    public class SysDictDataRepository
+    public class SysDictDataRepository : BaseRepository<SysDictData, int>, ISysDictDataRepository
     {
-        public SysDictDataRepository()
+        private YouGeDbContextOption option { get; set; }
+        public IYouGeDbContext _dataContext;
+        public SysDictDataRepository(IYouGeDbContext dbContext) : base(dbContext)
         {
+            _dataContext = dbContext;
+            option = (YouGeDbContextOption)DbContext.Option;
+        }
+
+        public List<SysDictData> selectDictDataList(SysDictData dictData)
+        {
+            //
+            throw new NotImplementedException();
+        }
+
+        public object selectDictDataById(long dictCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int insertDictData(SysDictData dict)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int updateDictData(SysDictData dict)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int deleteDictDataByIds(long[] dictCodes)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- YouGe.Core.Services/Sys/SysDictDataService.cs
+++ YouGe.Core.Services/Sys/SysDictDataService.cs
@@ -1,10 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+using YouGe.Core.Common.Helper;
+using YouGe.Core.Common.SystemConst;
+using YouGe.Core.Common.YouGeException;
+using YouGe.Core.Commons;
+using YouGe.Core.Commons.Helper;
+using YouGe.Core.DBEntitys.Sys;
+using YouGe.Core.Interface.IRepositorys.Sys;
+using YouGe.Core.Interface.IServices.Sys;
+using YouGe.Core.Models.DTModel.Sys;
+using YouGe.Core.Models.System;
+
 namespace YouGe.Core.Services.Sys
 {
-    public class SysDictDataService
+    public class SysDictDataService : ISysDictDataService
     {
         public SysDictDataService()
         {
+        }
+
+        public int deleteDictDataByIds(long[] dictCodes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int insertDictData(SysDictData dict)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object selectDictDataById(long dictCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<SysDictData> selectDictDataList(SysDictData dictData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int updateDictData(SysDictData dict)
+        {
+            throw new NotImplementedException();
         }
     }
 }
