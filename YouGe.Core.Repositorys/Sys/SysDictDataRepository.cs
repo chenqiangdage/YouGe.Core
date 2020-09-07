d27a90d3eb4ffb8c9dc4cb62ddc754e5a75f396f
chen qiang <chenqiangdage@foxmail.com>
2020-09-07 22:56:14 +0800

    每日坚持一个提交

--- YouGe.Core.Repositorys/Sys/SysDictDataRepository.cs
+++ YouGe.Core.Repositorys/Sys/SysDictDataRepository.cs
@@ -28,28 +28,48 @@ namespace YouGe.Core.Repositorys.Sys
             _dataContext = dbContext;
             option = (YouGeDbContextOption)DbContext.Option;
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dictData"></param>
+        /// <returns></returns>
         public List<SysDictData> selectDictDataList(SysDictData dictData)
         {
             //
             throw new NotImplementedException();
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dictCode"></param>
+        /// <returns></returns>
         public object selectDictDataById(long dictCode)
         {
             throw new NotImplementedException();
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dict"></param>
+        /// <returns></returns>
         public int insertDictData(SysDictData dict)
         {
             throw new NotImplementedException();
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dict"></param>
+        /// <returns></returns>
         public int updateDictData(SysDictData dict)
         {
             throw new NotImplementedException();
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dictCodes"></param>
+        /// <returns></returns>
         public int deleteDictDataByIds(long[] dictCodes)
         {
             throw new NotImplementedException();
